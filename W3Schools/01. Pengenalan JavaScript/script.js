b22eb9cfadd863f5e0735ef261df7dc9e6950055
putusujane <blogibagi@gmail.com>
2022-10-25 00:27:39 +0700

    Pengenalan JavaScript

---
+++ W3Schools/01. Pengenalan JavaScript/script.js
@@ -0,0 +1,16 @@
+document.getElementById("pengenalan").innerHTML =
+"JavaScript adalah bahasa pemrograman paling populer di dunia. <br>" +
+"JavaScript adalah bahasa pemrograman Web. <br>" + "JavaScript mudah dipelajari. <br>" +
+"Tutorial ini akan mengajarkan Anda JavaScript dari dasar hingga lanjutan.";
+
+document.getElementById("alasan").innerHTML
+= "JavaScript adalah salah satu dari 3 bahasa yang harus dipelajari semua pengembang web : <br> <br>" +
+"1. <strong>HTML</strong> untuk mendefinisikan konten halaman web. <br>" +
+"2. <strong>CSS</strong> untuk menentukan tata letak halaman web. <br>" + 
+"3. <strong>JavaScript</strong> untuk memprogram perilaku halaman web.";
+
+document.getElementById("kecepatanBelajar").innerHTML
+= "Dalam tutorial ini, kecepatan belajar adalah pilihan anda. <br>" +
+"Semua terserah pada kita. <br>" +
+"Jika kesulitan, istirahatlah, atau baca kembali materinya. <br>" +
+"Satu-satunya cara untuk menjadi programmer yang pintar adalah dengan: <strong>Berlatih. Praktik. Praktik. Koding. Koding. Koding!.</strong>";
