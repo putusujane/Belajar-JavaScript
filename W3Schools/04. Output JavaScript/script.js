3a8a608f673433d7a794b319070223836342061e
putusujane <blogibagi@gmail.com>
2022-10-26 23:02:20 +0700

    JavaScript Output

---
+++ W3Schools/04. Output JavaScript/script.js
@@ -0,0 +1,21 @@
+// Output JavaScript
+
+// Menggunakan innerHTML
+document.getElementById("divisi1").innerHTML = 5 + 6;
+
+// Menggunakan document.write()
+document.write("Hello, World!");
+document.writeln(1 + 3);
+document.writeln(1 + 4);
+
+// Menggunakan window.alert()
+window.alert(5 + 4);
+
+alert(100 + 2); // Bisa juga menggunakan seperti ini.
+
+// Menampilkan di console browser
+console.log(4 + 4);
+console.log("Hello, world!");
+
+// Mencetak konten
+// window.print();
