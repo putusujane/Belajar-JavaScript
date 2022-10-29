b4884f22ef9dbde3532c60b195d4d7d7f5c8e7ef
putusujane <blogibagi@gmail.com>
2022-10-29 12:33:58 +0700

    Belajar JS di YouTube DevTown

---
+++ belajar-js-DevTown.js
@@ -0,0 +1,17 @@
+// Mengambil input user
+var name = prompt("What is your name?");
+
+// Menampilkan notifikasi data di browser
+window.alert("Hi " + name + ", Welcome to DevTown.");
+
+// Menampilkan data di terminal atau console
+console.log("Hello, World!");
+
+// Declaring Function (Fungsi)
+function hello(age) {
+    var x = prompt("Ketik halo!");
+    alert("Kamu mengetik " + x);
+    return age / 2;
+}
+hello(27);
+
