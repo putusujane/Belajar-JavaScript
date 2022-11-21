57174c72c95bedb54f495052d9896a8f73ab97df
putusujane <blogibagi@gmail.com>
2022-11-21 13:52:42 +0700

    Menambah Ekspresi Regular

--- W3Schools/32. Regular Expressions/script.js
+++ W3Schools/32. Regular Expressions/script.js
@@ -9,7 +9,21 @@ document.getElementById("demo1").innerHTML = hasil1;
 
 // Mencari Kata Didalam String dan Menggantinya
 
+// mencari tanpa case-sensitive (bebas)
 function ganti() {
     let dataStr2 = document.getElementById("demo2").innerHTML;
-    document.getElementById("demo2").innerHTML = dataStr2.replace(/Microsoft/i, "W3Schools");
+    document.getElementById("demo2").innerHTML = dataStr2.replace(/Microsoft/i, "w3schools");
 }
+
+
+// Reguler Ekspresi Menggunakan g (harus case-sensitive)
+
+let str1 = "Hello, World! Hello, World! Hello, World! Hello, World!";
+document.writeln(str1.search(/World/g));
+
+document.writeln(str1.replace(/World!/g, "Dunia!"));
+
+document.writeln("<br>" + str1);
+
+// Multiline Matcing
+document.writeln("<br>" + str1.replace(/Hello/m, "Hay!"));
